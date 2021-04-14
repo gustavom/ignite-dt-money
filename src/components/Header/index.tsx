0af86db4bdfaec503c3d276140d7ca37e9017882
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2021-04-14 16:34:44 -0300

    add react modal on header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,25 @@
 import logo from '../../assets/logo.svg';
+import Modal from 'react-modal'
 import { Container, Content } from "./styles";
+import { useState } from 'react';
 
 export function Header(){
+  const [isNewTransactionOpen, setIsNewTransactionOpen] = useState(false);
+
+  function handleOpenNewTransactionModal(){
+    setIsNewTransactionOpen(true)
+  }
+  function handleCloseNewTransactionModal(){
+    setIsNewTransactionOpen(false)
+  }
   return(
     <Container>
       <Content>
       <img src={logo} alt="dt money"/>
-      <button>Nova Transação</button>
+      <button type="button" onClick={handleOpenNewTransactionModal}>Nova Transação</button>
+      <Modal isOpen={isNewTransactionOpen} onRequestClose={handleCloseNewTransactionModal}>
+        Nova transação:
+      </Modal>
       </Content>
     </Container>
   )
