32f7dfbcc89f46ec665baacd5e521b0ee5f6e18d
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2021-04-15 00:49:35 -0300

    add input colors var

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -14,6 +14,9 @@ export const GlobalStyle = createGlobalStyle`
     --text-body: #969cb3;
 
     --shape: #ffffff;
+
+    --input-border: #d7d7d7;
+    --input-bg: #e7e9ee;
   }
   
   *{
@@ -52,5 +55,26 @@ export const GlobalStyle = createGlobalStyle`
   [disabled]{
     opacity: 0.6;
     cursor: not-allowed;
+  }
+
+  .react-modal-overlay{
+    background: rgba(0,0,0,0.5);
+    position:fixed;
+    top:0;
+    bottom:0;
+    left:0;
+    right:0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .react-modal-content{
+    width: 100%;
+    max-width: 576px;
+    background: var(--background);
+    padding: 3rem;
+    position:relative;
+    border-radius:0.25rem;
   }
 `
