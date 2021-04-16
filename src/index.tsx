582aba4522dccb6be25d48b36564b1d81fa73a70
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2021-04-16 17:35:22 -0300

    create method post and create seed

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createServer} from 'miragejs'
+import {createServer, Model} from 'miragejs'
 import {App} from './App';
 
 createServer({
+  models:{
+    transaction: Model
+  },
+
+  seeds(server){
+    server.db.loadData({
+      transactions:[
+        {
+          id:1,
+          title: 'Freelance web',
+          type: 'deposit',
+          category: 'dev',
+          amount: 3000,
+          createdAt: new Date('2021-04-16 09:00:00')
+        },
+        {
+          id:2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'home',
+          amount: 1000,
+          createdAt: new Date('2021-04-19 10:00:00')
+        },
+      ]
+    })
+  },
+
   routes(){
     this.namespace = 'api';
 
     this.get('/transactions',()=>{
-      return[
-        {
-          id: 1,
-          title: 'Transaction 01',
-          type: 'deposit',
-          category: 'Food',
-          amount: 400,
-          createdAt: new Date()
-        }
-      ]
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request)=>{
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transaction', data);
     })
   }
 })
